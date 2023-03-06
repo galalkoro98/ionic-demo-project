c346021351651fe077ffdb91c0ec89e408cbfdde
galalkoro98 <galal.koro@gmail.com>
2023-03-06 21:57:18 +0100

    add level landing page

--- src/pages/Level1.tsx
+++ src/pages/Level1.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { IonPage, IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 import './styles/level.css'
 
-export const Level1: React.FC = () => {
+import { LevelOne } from '../components/LevelOne'
+import { page } from '../hooks/customHooks';
 
+export const Level1: React.FC = () => {
+    const { showQuiz, startQuiz } = page()
+    
     return (
         <IonPage>
             <IonContent fullscreen>
                 <div className="background">
                     <div className="ion-text-center ion-padding">
-                        <IonCard>
-                            <img alt="Silhouette of mountains" src="../../assets/icon/cover.jpg" />
-                            <IonCardHeader>
-                                <IonCardTitle color="secondary" >Level 1</IonCardTitle>
-                            </IonCardHeader>
-                            <IonCardContent className='first__level__description'>
-                                Test your knowledge with our fun and challenging quiz. The quiz contains a series of questions designed to test your understanding of the subject matter.
-                            </IonCardContent>
-                        </IonCard>
+                        {showQuiz ? (
+                            <LevelOne />
+                        ) : (
+                            <IonCard>
+                                <img alt="Silhouette of mountains" src="../../assets/icon/cover.jpg" />
+                                <IonCardHeader>
+                                    <IonCardTitle color="secondary" >Level 1</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent className='first__level__description'>
+                                    Test your knowledge with our fun and challenging quiz. The quiz contains a series of questions designed to test your understanding of the subject matter.
+                                </IonCardContent>
+                            </IonCard>
+                        )}
                         <IonButton color="primary" className="ion-margin-top" routerLink="/home"> Back</IonButton>
-                        <IonButton color="primary" className="ion-margin-top">Start Level 1</IonButton>
+                        <IonButton color="primary" className="ion-margin-top" type='button' onClick={startQuiz}>Start Level 1</IonButton>
                         <IonButton color="primary" className="ion-margin-top" routerLink="/level2"> Next Level</IonButton>
                     </div>
                 </div>
--- src/pages/Level2.tsx
+++ src/pages/Level2.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { IonPage, IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 
+import { LevelTwo } from '../components/LevelTwo';
+import { page } from '../hooks/customHooks';
 
 export const Level2: React.FC = () => {
+    const { showQuiz, startQuiz } = page()
 
     return (
         <IonPage>
             <IonContent fullscreen>
                 <div className="background">
                     <div className="ion-text-center ion-padding">
-                        <IonCard>
-                            <img alt="Silhouette of mountains" src="../../assets/icon/html.png" />
-                            <IonCardHeader>
-                                <IonCardTitle color="secondary" >Level 2</IonCardTitle>
-                            </IonCardHeader>
-                            <IonCardContent className='first__level__description'>
-                                HTML stands for Hypertext Markup Language and is the standard markup language used to create and design web pages. HTML consists of a series of elements and tags that define the structure and content of a web page
-                            </IonCardContent>
-                        </IonCard>
+                        {showQuiz ? (
+                            <LevelTwo />
+                        ) : (
+                            <IonCard>
+                                <img alt="Silhouette of mountains" src="../../assets/icon/html.png" />
+                                <IonCardHeader>
+                                    <IonCardTitle color="secondary" >Level 2</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent className='first__level__description'>
+                                    HTML stands for Hypertext Markup Language and is the standard markup language used to create and design web pages. HTML consists of a series of elements and tags that define the structure and content of a web page
+                                </IonCardContent>
+                            </IonCard>
+                        )}
                         <IonButton color="primary" className="ion-margin-top" routerLink="/level1">Back</IonButton>
-                        <IonButton color="primary" className="ion-margin-top" > Start Level 2 </IonButton>
+                        <IonButton color="primary" className="ion-margin-top" onClick={startQuiz}> Start Level 2 </IonButton>
                         <IonButton color="primary" className="ion-margin-top" routerLink="/level3"> Next Level </IonButton>
                         <IonButton color="primary" className="ion-margin-top" routerLink="/level3"> Home </IonButton>
                     </div>
