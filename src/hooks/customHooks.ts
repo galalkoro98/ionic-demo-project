ebbe8fa3bc1f3299df647cf69de734e486475509
galalkoro98 <galal.koro@gmail.com>
2023-03-10 01:42:22 +0100

    add new functionalty

--- src/hooks/customHooks.ts
+++ src/hooks/customHooks.ts
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export const page = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [showQuiz, setShowQuiz] = useState(false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [quizFinished, setQuizFinished] = React.useState(false);
 
     const startQuiz = () => {
+        setQuizFinished(false);
         setShowQuiz(true);
-
     }
 
     return {
         showQuiz,
-        startQuiz
+        startQuiz,
+        quizFinished,
+        setQuizFinished
     }
 }
